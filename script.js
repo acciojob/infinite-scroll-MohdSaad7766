8a892d5350c4e2fb918983bee3c13e427fd7796b
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-07-06 13:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+const list = document.getElementById('infi-list');
+let count = 1;
 
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement('li');
+    li.textContent = `Item ${count++}`;
+    list.appendChild(li);
+  }
+}
+
+// Initial 10 items
+addItems(10);
+
+// Infinite scroll
+list.addEventListener('scroll', () => {
+  const buffer = 10;
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight - buffer) {
+    addItems(2);
+  }
+});
